e13c034ddbbc223d414269ec4bb2b1b6627d9cd9
RMYazid94 <mohamedyazid.rouame@gmail.com>
2024-04-19 20:15:30 +0100

    6 commit

--- projetecom/src/DetailProduct.js
+++ projetecom/src/DetailProduct.js
@@ -1,20 +1,27 @@
 import React from 'react'
-import Product from './Product';
+import Button from 'react-bootstrap/Button';
 
-function DetailProduct({Products}) {
+function DetailProduct({}) {
 return (
-<>
+<section className="d-flex flex-column">
+<div className='d-flex align-items-center'>
+  <div className="flex-shrink-0 ms-3 ">
+    <img src="" alt="image"></img>
+  </div>
+  <div class="flex-grow-1 ms-3">
+     <h2>Name Product</h2>
+     <p>Price</p>
+     <input placeholder='1'></input>
+     <Button as="input" type="submit" value="Submit" />{' '}
+  </div>
+ </div>
 <div>
-{Products.map((Product) =>(
-<img src={Product.ImageUrl} />
-<h1>{Product.Name}</h1>
-<p>{Product.Price}</p>
-
-))}
-</div>
-<input></input>
-</>
-    )
-}
+    <h4>About this Product</h4>
+    <p>Description du produit</p>
+    <Button variant="danger">Delete Product</Button>{' '}
+    </div>
+</section>
+)
+};
 
 export default DetailProduct;
