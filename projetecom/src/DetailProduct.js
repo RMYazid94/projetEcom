dfb92cb37072cfdf677e34bb5cef60f51df581d2
RMYazid94 <mohamedyazid.rouame@gmail.com>
2024-04-20 14:39:04 +0100

    6 commit

--- projetecom/src/DetailProduct.js
+++ projetecom/src/DetailProduct.js
@@ -3,9 +3,9 @@ import Button from 'react-bootstrap/Button';
 
 function DetailProduct({}) {
 return (
-<section className="d-flex flex-column">
-<div className='d-flex align-items-center'>
-  <div className="flex-shrink-0 ms-3 ">
+<section className="d-flex flex-column w-100">
+<div className='d-flex justify-content-around w-100'>
+  <div className="flex-shrink-0 ms-3  w-50">
     <img src="" alt="image"></img>
   </div>
   <div class="flex-grow-1 ms-3">
